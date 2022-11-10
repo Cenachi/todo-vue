de7dc26bdcfb774d195d162299397fd9131726ba
Cenachi <carloscenachi@protonmail.com>
2022-11-10 02:21:25 -0300

    sass-install, input-bpttom-style

--- src/components/TodoList.vue
+++ src/components/TodoList.vue
@@ -5,14 +5,48 @@ import { useToDo } from "../store/todo";
 
 <template>
   <section class="todo-list">
-    <h1>Tasks</h1>
+    <h3>Tasks</h3>
 
-    <div class="are-task"></div>
+    <div class="are-task">
+      <p>Tasks show up here</p>
+    </div>
 
-    <input type="text" placeholder="New task" />
-    <button class="btn-add">+</button>
+    <div class="input-btn">
+      <input type="text" placeholder="New task" />
+      <button class="btn-add">+</button>
+    </div>
+
     <button class="btn-delete">Delete All</button>
   </section>
 </template>
 
-<style scoped></style>
+<style scoped lang="scss">
+.input-btn {
+  /* padding: 8px; */
+  background: #242424;
+
+  input,
+  .btn-add {
+    outline: none;
+    background: none;
+    border: none;
+    border-bottom: 1px solid #ddd;
+    padding: 8px;
+    align-items: left;
+  }
+}
+.btn-delete {
+  margin-top: 10px;
+  padding: 8px;
+  background: #ff0000;
+  border: none;
+  border-radius: 4px;
+  cursor: pointer;
+}
+
+.btn-add:hover {
+  background: #ddd;
+  transition: 0.2s;
+  cursor: pointer;
+}
+</style>
